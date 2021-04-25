50df0a3184c23a4c7a0951cefb2a8e82e37c5da3
kilhyeongyeong <51154237+kilhyeongyeong@users.noreply.github.com>
2021-04-25 16:41:02 +0900

    Add files via upload

---
+++ ConsoleApp08loop3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp08loop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                Console.WriteLine("종료시 [x]입력 : ");
+                keyInfo = Console.ReadKey();
+            } while (keyInfo.Key != ConsoleKey.X);
+
+            do
+            {
+                
+            } while (keyInfo.Key != ConsoleKey.X);
+        }
+
+
+    }
+}
